3bbe9f481e1f02a91be1093a5ac628db7f484e9e
soethandar90 <soethandar18@gmail.com>
2022-11-24 16:00:48 -0600

    Remove comments from artistcontroller

--- projects/php/controller/artist.controller.js
+++ projects/php/controller/artist.controller.js
@@ -11,22 +11,6 @@ const getAll = function (req, res) {
     if (req.query && req.query.count) {
         count = parseInt(req.query.count, process.env.MAXIMUM_OFFSET);
     }
-    // Artists.find()
-    //     .skip(offset)
-    //     .limit(count)
-    //     .sort([['_id', -1]]).exec(function (err, result) {
-    //         const response = { status: parseInt(process.env.OK_STATUS_CODE), message: result };
-    //         if (err) {
-    //             console.log(process.env.FIND_ERROR_MESSAGE);
-    //             response.status = parseInt(process.env.SYSTEM_ERROR_STATUS_CODE);
-    //             response.message = err;
-    //         }
-    //         console.log(process.env.FOUND_MUSIC_COLLECTION_MESSAGE, result.length);
-    //         res.status(parseInt(process.env.OK_STATUS_CODE)).json(result);
-    //     });
-
-    //Replacing callbacks with promises
-    console.log("Hello");
     const response = { status: parseInt(process.env.OK_STATUS_CODE), message: "" };
     Artists.find().skip(offset).limit(count).sort([['_id', -1]]).exec()
         .then((result) => {
@@ -36,24 +20,11 @@ const getAll = function (req, res) {
             console.log(process.env.FIND_ERROR_MESSAGE);
             response.status = parseInt(process.env.SYSTEM_ERROR_STATUS_CODE);
             response.message = err;
-        })
-        // .finally(() => {
-        //     res.status(response.status).json(response.message);
-        // })
-        ;
+        });
 }
 
 const getOne = function (req, res) {
     const artistId = req.params.artistId;
-    // Artists.findById(artistId).exec(function (err, result) {
-    //     const response = { status: parseInt(process.env.OK_STATUS_CODE), message: result };
-    //     if (err) {
-    //         console.log(process.env.FIND_ERROR_MESSAGE);
-    //         response.status = parseInt(process.env.SYSTEM_ERROR_STATUS_CODE);
-    //         response.message = err;
-    //     }
-    //     res.status(response.status).json(result);
-    // });
     const response = { status: parseInt(process.env.OK_STATUS_CODE), message: "" };
     Artists.findById(artistId).exec()
         .then((result) => {
@@ -63,11 +34,7 @@ const getOne = function (req, res) {
             console.log(process.env.FIND_ERROR_MESSAGE);
             response.status = parseInt(process.env.SYSTEM_ERROR_STATUS_CODE);
             response.message = err;
-        })
-        //     .finally(() => {
-        //         res.status(response.status).json(response.message);
-        //     })
-        ;
+        });
 }
 
 const addOne = function (req, res) {
@@ -77,16 +44,6 @@ const addOne = function (req, res) {
         dob: req.body.dob,
         album: req.body.album
     };
-    // Artists.create(newArtist, function (err, result) {
-    //     const response = { status: parseInt(process.env.SUCCESS_STATUS_CODE), message: result };
-    //     if (err) {
-    //         console.log(process.env.ADD_ONE_ERROR_MESSAGE);
-    //         response.status = parseInt(process.env.SYSTEM_ERROR_STATUS_CODE);
-    //         response.message = err;
-    //     }
-    //     res.status(response.status).json(response.message);
-    // })
-
     const response = { status: parseInt(process.env.SUCCESS_STATUS_CODE), message: "" };
     Artists.create(newArtist)
         .then((result) => {
@@ -99,43 +56,9 @@ const addOne = function (req, res) {
         });
 }
 
-// const addAll = function (req, res) {
-//     //console.log(process.env.ADD_ONE_REQUEST_RECEIVED_MESSAGE)
-//     const newArtist = [{
-//         name: req.body.name,
-//         dob: req.body.dob,
-//         album: [req.body.album]
-//     }];
-//     Artists.create(newArtist, function (err, result) {
-//         const response = { status: parseInt(process.env.SUCCESS_STATUS_CODE), message: result };
-//         if (err) {
-//             console.log(process.env.ADD_ONE_ERROR_MESSAGE);
-//             response.status = parseInt(process.env.SYSTEM_ERROR_STATUS_CODE);
-//             response.message = err;
-//         }
-//         res.status(response.status).json(response.message);
-//     })
-// }
-
 const deleteOne = function (req, res) {
     console.log(process.env.DELETE_REQUEST_RECEIVED_MESSAGE)
     const artistId = req.params.artistId;
-    // Artists.findByIdAndDelete(artistId).exec(function (err, result) {
-    //     const response = { status: parseInt(process.env.SUCCESS_STATUS_CODE), message: result };
-    //     if (err) {
-    //         console.log(process.env.FIND_ERROR_MESSAGE);
-    //         response.status = parseInt(process.env.SYSTEM_ERROR_STATUS_CODE);
-    //         response.message = err;
-    //     } else if (!result) {
-    //         console.log(process.env.COLLECTION_NOT_FOUND_MESSAGE);
-    //         response.status = parseInt(process.env.CONTENT_NOT_FOUND_STATUS_CODE);
-    //         response.message = {
-    //             "message": process.env.COLLECTION_NOT_FOUND_MESSAGE
-    //         };
-    //     }
-    //     res.status(response.status).json(response.message);
-    // });
-
     const response = { status: parseInt(process.env.SUCCESS_STATUS_CODE), message: "" };
     Artists.findByIdAndDelete(artistId).exec()
         .then((result) => {
@@ -154,47 +77,10 @@ const deleteOne = function (req, res) {
             response.message = err;
         });
 }
-/** 
-const updateOne = function (req, res) {
-    console.log(process.env.UPDATE_ONE_REQUEST_RECEIVED_MESSAGE);
-    const artistId = req.params.artistId;
-    const newMusicCollection = {
-        name: req.body.name,
-        dob: req.body.dob,
-        album: req.body.album
-    };
-    MusicCollection.findByIdAndUpdate(artistId, newMusicCollection).exec(function (err, result) {
-        const response = { status: parseInt(process.env.SUCCESS_STATUS_CODE), message: result };
-        if (err) {
-            console.log(process.env.UPDATE_ERROR_MESSAGE);
-            response.status = parseInt(process.env.SYSTEM_ERROR_STATUS_CODE);
-            response.message = err;
-        }
-        res.status(response.status).json(response.message);
-    });
-}*/
 
 const _updateOne = function (req, res, updateMcCallback) {
     console.log("Update One Artist Controller");
     const artistId = req.params.artistId;
-    // Artists.findById(artistId).exec(function (err, mc) {
-    //     const response = { status: parseInt(process.env.SUCCESS_STATUS_CODE), message: mc };
-    //     if (err) {
-    //         console.log("Error finding Music Collection");
-    //         response.status = parseInt(process.env.SYSTEM_ERROR_STATUS_CODE);
-    //         response.message = err;
-    //     } else if (!mc) {
-    //         console.log("Music Collection ID not found");
-    //         response.status = parseInt(process.env.CONTENT_NOT_FOUND_STATUS_CODE);
-    //         response.message = { "message": "Music Collection ID not found" };
-    //     }
-    //     if (response.status !== parseInt(process.env.SUCCESS_STATUS_CODE)) {
-    //         res.status(response.status).json(response.message);
-    //     } else {
-    //         updateMcCallback(req, res, mc, response);
-    //     }
-    // });
-
     const response = { status: parseInt(process.env.SUCCESS_STATUS_CODE), message: "" };
     Artists.findById(artistId).exec()
         .then((result) => {
@@ -212,13 +98,6 @@ const _updateOne = function (req, res, updateMcCallback) {
             response.message = err;
             res.status(response.status).json(response.message);
         })
-
-    // if (response.status !== parseInt(process.env.SUCCESS_STATUS_CODE)) {
-    //     res.status(response.status).json(response.message);
-    // } else {
-    //     updateMcCallback(req, res, result, response);
-    // }
-
 }
 
 const fullUpdateOne = function (req, res) {
@@ -227,14 +106,6 @@ const fullUpdateOne = function (req, res) {
         mc.name = req.body.name;
         mc.dob = req.body.dob;
         mc.album = req.body.album;
-        // mc.save(function (err, updatedMc) {
-        //     if (err) {
-        //         response.status = parseInt(process.env.SYSTEM_ERROR_STATUS_CODE);
-        //         response.message = err;
-        //     }
-        //     res.status(response.status).json(response.message);
-        // });
-
         mc.save()
             .then((result) => {
                 res.status(response.status).json(response.message);
@@ -260,15 +131,6 @@ const partialUpdateOne = function (req, res) {
         if (req.body.album) {
             mc.album = req.body.album;
         }
-        // mc.save(function (err, updatedMc) {
-        //     if (err) {
-        //         response.status = parseInt(process.env.SYSTEM_ERROR_STATUS_CODE);
-        //         response.message = err;
-        //     }
-        //     res.status
-        //         (response.status).json(response.message);
-        // });
-
         mc.save()
             .then((result) => {
                 res.status(response.status).json(response.message);
@@ -286,7 +148,6 @@ module.exports = {
     getAll: getAll,
     getOne: getOne,
     addOne: addOne,
-    //addAll: addAll,
     deleteOne: deleteOne,
     fullUpdateOne: fullUpdateOne,
     partialUpdateOne: partialUpdateOne
